d7674082a9e082b9689a64cadcb0b2b715779ea6
Milan Mody <milanm1120@gmail.com>
2021-02-11 02:17:32 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -2,7 +2,7 @@ require "pry"
 
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(argument = "Squawk!")
+def parrot(argument)
   binding.pry
   puts "#{argument}"
   argument
